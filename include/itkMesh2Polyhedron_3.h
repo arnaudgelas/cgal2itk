7daf1a5611762e70c5d1153509ea4e51a9015ae7
Arnaud Gelas <arnaudgelas@gmail.com>
2014-04-25 08:40:39 +0200

    test conversion itk mesh to cgal's

--- include/itkMesh2Polyhedron_3.h
+++ include/itkMesh2Polyhedron_3.h
@@ -1,11 +1,14 @@
 #ifndef __itkMesh2CGALPolyhedron_3_h
 #define __itkMesh2CGALPolyhedron_3_h
 
+#include <CGAL/Polyhedron_incremental_builder_3.h>
+
 template< class TITKMesh, class TCGALMesh >
 class Convert
 {
 public:
   typedef TITKMesh                                            ITKMeshType;
+  typedef typename ITKMeshType::Pointer                  ITKMeshPointer;
   typedef typename ITKMeshType::ConstPointer                  ITKMeshConstPointer;
 
   typedef typename ITKMeshType::PointType                     ITKPointType;
@@ -20,15 +23,15 @@ public:
   typedef TCGALMesh                         CGALMeshType;
   typedef typename CGALMeshType::HalfedgeDS CGALHalfEdgeType;
   typedef typename CGALHalfEdgeType::Vertex CGALVertexType;
-  typedef typename CGALHalfEdgeType::Point  CGALPointType;
+  typedef typename CGALVertexType::Point  CGALPointType;
 
-  static void fromITKtoCGAL( const ITKMeshType* input, CGALMeshType& oMesh ) const
+  static void fromITKtoCGAL( ITKMeshType* input, CGALMeshType& oMesh )
   {
     cgal_surface_mesh_builder builder( input );
     oMesh.delegate( builder );
   }
 
-  static void fromCGALToITK( const CGALMeshType& input, ITKMeshType* oMesh ) const
+  static void fromCGALToITK( const CGALMeshType& input, ITKMeshType* oMesh )
   {
     
   }
@@ -39,18 +42,20 @@ protected:
 
 private:
 
-  class cgal_surface_mesh_builder : public CGAL::Modifier_base<  >
+  class cgal_surface_mesh_builder : public CGAL::Modifier_base< CGALHalfEdgeType >
   {
   public:
-    ty
-    cgal_surface_mesh_builder( const ITKMeshType* iMesh ) : m_Mesh( iMesh ) {}
+    cgal_surface_mesh_builder( ITKMeshType* iMesh )
+    {
+       m_Mesh = iMesh;
+    }
 
     void operator() ( CGALHalfEdgeType& hds )
     {
       CGAL::Polyhedron_incremental_builder_3< CGALHalfEdgeType > builder( hds, true );
       builder.begin_surface(  m_Mesh->GetNumberOfPoints(),
-                              m_Mesh->GetNumberOfFaces(),
-                              2 * m_Mesh->GetNumberOfEdges() );
+                              m_Mesh->GetNumberOfCells(),
+                              0 );
 
       typename TITKMesh::PointsContainerConstPointer points = m_Mesh->GetPoints();
       typename TITKMesh::PointsContainerConstIterator pIt = points->Begin();
@@ -59,7 +64,7 @@ private:
       while( pIt != pEnd )
       {
         ITKPointType p = pIt->Value();
-        builder.add_vertex( Point( p[0], p[1], p[2] ) );
+        builder.add_vertex( CGALPointType( p[0], p[1], p[2] ) );
         ++pIt;
       }
 
@@ -73,7 +78,7 @@ private:
 
         const ITKCellType* cell = cIt->Value();
 
-        typedef typename ITKCellType::PointIdIterator PointIdIterator;
+        typedef typename ITKCellType::PointIdConstIterator PointIdIterator;
         PointIdIterator faceIt = cell->PointIdsBegin();
         PointIdIterator faceEnd = cell->PointIdsEnd();
 
@@ -82,15 +87,15 @@ private:
           builder.add_vertex_to_facet( *faceIt );
           ++faceIt;
         }
-        builder._facet();
+        builder.end_facet();
 
         ++cIt;
       }
     builder.end_surface();
     }
-  };
   private:
-    ITKMeshConstPointer m_Mesh;
+    ITKMeshPointer m_Mesh;
+  };
 };
 #endif  __itkMesh2CGALPolyhedron_3_h
 
--- tests/itkMesh2CGALMesh.cxx
+++ tests/itkMesh2CGALMesh.cxx
@@ -1,4 +1,122 @@
+#include <itkRegularSphereMeshSource.h>
+#include <itkMesh.h>
+#include <itkQuadEdgeMesh.h>
+
+#include <CGAL/Cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+
+#include "itkMesh2Polyhedron_3.h"
+
+template< class TInputMesh, class TOutputMesh >
+int CompareMesh( TInputMesh* input, const TOutputMesh& oMesh )
+{
+  typename TInputMesh::PointsContainerConstPointer points = input->GetPoints();
+  typename TInputMesh::PointsContainerConstIterator pIt   = points->Begin();
+  typename TInputMesh::PointsContainerConstIterator pEnd  = points->End();
+
+  typename TOutputMesh::Vertex_const_iterator vIt   = oMesh.vertices_begin();
+  typename TOutputMesh::Vertex_const_iterator vEnd  = oMesh.vertices_end();
+
+  const double distanceThreshold = 1e-9;
+
+  std::map< typename TOutputMesh::Vertex_const_handle, typename TInputMesh::PointIdentifier > mapping;
+
+  while( ( pIt != pEnd ) && ( vIt != vEnd ) )
+  {
+    typename TInputMesh::PointType p = pIt->Value();
+
+    typename TInputMesh::PointType q;
+    q[0] = vIt->point().x();
+    q[1] = vIt->point().y();
+    q[2] = vIt->point().z();
+
+    mapping[ vIt ] = pIt->Index();
+
+    if( p.EuclideanDistanceTo( q ) > distanceThreshold )
+    {
+      std::cerr << "ITK point " << p << " != CGAL point " << q << std::endl;
+      return EXIT_FAILURE;
+    }
+    ++vIt;
+    ++pIt;
+  }
+
+  typename TInputMesh::CellsContainerConstPointer cells = input->GetCells();
+  typename TInputMesh::CellsContainerConstIterator cIt  = cells->Begin();
+  typename TInputMesh::CellsContainerConstIterator cEnd = cells->End();
+
+  typename TOutputMesh::Face_const_iterator fIt   = oMesh.facets_begin();
+  typename TOutputMesh::Face_const_iterator fEnd  = oMesh.facets_end();
+
+  while( ( cIt != cEnd ) && ( fIt != fEnd ) )
+  {
+    typename TOutputMesh::Halfedge_around_facet_const_circulator circulator = fIt->facet_begin();
+
+    std::set< typename TInputMesh::PointIdentifier > list;
+
+    do
+    {
+      typename TOutputMesh::Vertex_const_handle v = circulator->vertex();
+      list.insert( mapping[ v ] );
+    } while( circulator != fIt->facet_begin() );
+
+    typedef typename TInputMesh::CellType ITKCellType;
+    typedef typename ITKCellType::PointIdConstIterator PointIdIterator;
+
+    typename TInputMesh::CellType* cell = cIt->Value();
+
+    PointIdIterator faceIt  = cell->PointIdsBegin();
+    PointIdIterator faceEnd = cell->PointIdsEnd();
+
+    while( faceIt != faceEnd )
+    {
+      list.erase( *faceIt );
+      ++faceEnd;
+    }
+
+    if( !list.empty() )
+    {
+      std::cerr << "faces are different" << std::endl;
+      return EXIT_FAILURE;
+    }
+    ++fIt;
+    ++cIt;
+  }
+
+  return EXIT_SUCCESS;
+
+}
+
+template< class TInputMesh, class TOutputMesh >
+int LoadAndTestSphere()
+{
+  typedef itk::RegularSphereMeshSource< TInputMesh > SourceType;
+  typename SourceType::Pointer source = SourceType::New();
+
+  TOutputMesh oMesh;
+  Convert< TInputMesh, TOutputMesh >::fromITKtoCGAL( source->GetOutput(), oMesh );
+
+  return CompareMesh( source->GetOutput(), oMesh );
+}
+
 int main()
 {
-  return 0;
+  const unsigned int Dimension = 3;
+  typedef itk::Mesh< double, Dimension > ITKMesh3d;
+  typedef itk::Mesh< float, Dimension > ITKMesh3f;
+  typedef itk::QuadEdgeMesh< double, Dimension > ITKQEMesh3d;
+
+  typedef CGAL::Cartesian< double > Cartesiand;
+  typedef CGAL::Polyhedron_3< Cartesiand > CGALMeshd;
+
+  if( LoadAndTestSphere< ITKMesh3d, CGALMeshd >() == EXIT_FAILURE )
+  {
+    return EXIT_FAILURE;
+  }
+  if( LoadAndTestSphere< ITKMesh3f, CGALMeshd >() == EXIT_FAILURE )
+  {
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
